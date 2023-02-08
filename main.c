9e540b7da94c46e1320c136a6f664762b063d2a2
ensyprk <124585449+ensyprk@users.noreply.github.com>
2023-02-08 17:24:24 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int sayi = 10;
+	
+	printf("%d", karesi(sayi));
+	
+
+	return 0;
+}
+
+
+int karesi(int x)
+{
+x*=x;
+return x;	
+	
+}
+
